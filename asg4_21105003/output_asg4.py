048343ece53993868e7376272313839c896c9195
HarmanSingh_21105003 <46106438+Harman2003@users.noreply.github.com>
2022-03-02 00:20:28 +0530

    Add files via upload

---
+++ asg4_21105003/output_asg4.py
@@ -0,0 +1,90 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: D:\gurtaj singh\asg4_21105003\source_code.py ============
+Assignment 4
+
+
+Question 1
+
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+Question 2
+
+Enter n upto which pascal triangle is to be printed: 5
+
+ Recursive Way : 
+
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+ Iterative way : 
+
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+Question 3
+
+Enter the first integer: 17
+Enter the second integer: 9
+Remainder is:  8
+Quotient is:  1
+True, means it is callable
+Both values are non zero
+{12}
+{12, 5}
+{13, 12, 5}
+{13, 12, 5, 6}
+{5, 6, 12, 13, 14}
+{5, 6, 7, 12, 13, 14}
+{5, 6, 7, 12, 13, 14}
+Immutable set:  frozenset({5, 6, 7, 12, 13, 14})
+Largest value in the set:  14
+Hash value:  14
+
+
+Question 4
+
+Enter name of student: Harman Singh
+Enter SID of Harman Singh: 21105003
+Object has been initialised
+
+Student Name :  Harman Singh
+Roll No :  21105003
+
+Object has been destroyed
+ 
+ Question 5 
+
+Employee name :  Mehak
+Employee salary :  40000
+
+Employee name :  Ashok
+Employee salary :  50000
+
+Employee name :  Viren
+Employee salary :  60000
+
+The updated data of employee_1 is :
+Employee name :  Mehak
+Employee salary :  70000
+
+Record of Viren is deleted.
+
+ Question 6 
+
+Enter George's word: Listen
+Enter Barbie's guessed word: Silent
+Barbie and George are true friends
